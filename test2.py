02507defea224a554a8202ea50f4bf34b1efac7c
49831117 <73454248+49831117@users.noreply.github.com>
2021-01-30 17:03:14 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 
 #mouse callback function
-def draw_circle(event,x,y,flags,param):
+def draw_pic(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDBLCLK: # 滑鼠左鍵點兩下
-        cv2.circle(img, (x,y), 100, (255,0,0), -1)
+        cv2.ellipse(img, (x, y), (20, 50), 45, 0, 180, (255, 0, 255), -1)
 
 # 創建圖像與窗口並將窗口與回調函數綁定
 img = np.zeros((512,512,3),np.uint8)
 
 cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_circle)
+cv2.setMouseCallback('image',draw_pic)
 
 while(1):
     cv2.imshow('image',img)

99cc9236bc0ff59ea5be4cce6c779f080e9815ef
49831117 <73454248+49831117@users.noreply.github.com>
2021-01-30 16:31:12 +0800

    Update test12.py

--- test12.py
+++ test12.py
@@ -54,12 +54,35 @@ rf：https://shengyu7697.github.io/blog/2020/03/18/Python-OpenCV-canny/
 contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 draw = cv2.drawContours(img1, contours, -1, (200, 200, 0), 2)
 
-
 plt.subplot(131), plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB)), plt.title('Original Image')
 plt.subplot(132), plt.imshow(edges,cmap='gray'), plt.title('Edge Image')
 plt.subplot(133), plt.imshow(cv2.cvtColor(draw,cv2.COLOR_BGR2RGB)), plt.title('contour Image')
 
 plt.show()
+
+############################
+
+def nothing(x):
+    pass
+
+edges = cv2.Canny(img, 50, 100)   
+cv2.namedWindow("image")
+cv2.createTrackbar('minval', 'image', 0, 255, nothing)
+cv2.createTrackbar('maxval', 'image', 0, 255, nothing)
+
+
+while(1):
+    cv2.imshow('image', edges)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+    minval = cv2.getTrackbarPos('minval', 'image')
+    maxval = cv2.getTrackbarPos('maxval', 'image')
+    edges = cv2.Canny(img, minval, maxval)
+
+############################
+
+
 
 
 '''
